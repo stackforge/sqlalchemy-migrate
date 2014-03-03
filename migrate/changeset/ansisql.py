ab3af39a3764c5f8511633bf63469851d7119a7f
Thomas Goirand <thomas@goirand.fr>
2014-03-03 11:11:06 +0800

    Adds another SQLA 9 quote fix
    In migrate/changeset/ansisql.py line 234 and 257, we are
    still seeing some deprecation warnings, which lead to unit
    tests failing. This patch fixes that.
    
    Change-Id: I5a8694d405e9dc1b08bb3f254ee20c16ff1125b1

--- migrate/changeset/ansisql.py
+++ migrate/changeset/ansisql.py
@@ -157,8 +157,8 @@ class ANSISchemaChanger(AlterTableVisitor, SchemaGenerator):
     def visit_table(self, table):
         """Rename a table. Other ops aren't supported."""
         self.start_alter_table(table)
-        sqla_majv,sqla_medv,sqla_minv = sqlalchemy.__version__.split('.')
-        if int(sqla_medv) < 9:
+        sqla_majv, sqla_medv, sqla_minv = sqlalchemy.__version__.split('.')
+        if int(sqla_majv) == 0 and int(sqla_medv) < 9:
             q = table.quote
         else:
             q = table.name.quote
@@ -231,7 +231,12 @@ class ANSISchemaChanger(AlterTableVisitor, SchemaGenerator):
     def start_alter_column(self, table, col_name):
         """Starts ALTER COLUMN"""
         self.start_alter_table(table)
-        self.append("ALTER COLUMN %s " % self.preparer.quote(col_name, table.quote))
+        sqla_majv,sqla_medv,sqla_minv = sqlalchemy.__version__.split('.')
+        if int(sqla_medv) < 9:
+            q = table.quote
+        else:
+            q = table.name.quote
+        self.append("ALTER COLUMN %s " % self.preparer.quote(col_name, q))
 
     def _visit_column_nullable(self, table, column, delta):
         nullable = delta['nullable']
@@ -254,7 +259,12 @@ class ANSISchemaChanger(AlterTableVisitor, SchemaGenerator):
 
     def _visit_column_name(self, table, column, delta):
         self.start_alter_table(table)
-        col_name = self.preparer.quote(delta.current_name, table.quote)
+        sqla_majv, sqla_medv, sqla_minv = sqlalchemy.__version__.split('.')
+        if int(sqla_majv) == 0 and int(sqla_medv) < 9:
+            q = table.quote
+        else:
+            q = table.name.quote
+        col_name = self.preparer.quote(delta.current_name, q)
         new_name = self.preparer.format_column(delta.result_column)
         self.append('RENAME COLUMN %s TO %s' % (col_name, new_name))
 
